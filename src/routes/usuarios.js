9f433a57a2a4e6ff0841ffb574b06b3fe7ca22bf
MatiasIb <matiasrolando2003@gmail.com>
2022-11-02 17:09:03 -0300

    a

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -6,16 +6,18 @@ const router = express.Router()
 //createUser
 router.post('/usuarios', (req, res) => {
     const user = usuarioSchema(req.body);
-    user.save().then((data) => res.json(data)).catch((error) => res.json({ message:error }))
+    user.save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message:error }))
     
 } )
 
 router.get('/usuarios', (req, res) => {
-    res.json({
-        nombre:"admin",
-        clave:"admin",
-        cargo:"admin"
-    })
+    usuarioSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message:error }))
+    
 } )
 
 
