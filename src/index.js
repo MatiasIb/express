311339dfbc4aad0c67bc2a200f806c9301b6377b
MatiasIb <matiasrolando2003@gmail.com>
2022-12-25 22:31:09 -0300

    final

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use(cors())
 app.use('/api', userRoutes)
 app.use('/api/estructura/', areaRoutes)
 app.use((req, res, next) => {
-  res.send('FUNCIONA')
+  res.send('no existe esa ruta')
   next()
 })
 
